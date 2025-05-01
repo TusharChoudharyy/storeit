780e76990599361ec53e3f4a819751ee73bac580
TusharChoudharyy <chtushardev@gmail.com>
2025-05-02 01:00:16 +0530

    storeit

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-poppins'
+})
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "StoreIt",
+  description: "StoreIt - The only storage solution you need",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.variable} font-poppins antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
